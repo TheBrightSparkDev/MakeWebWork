5e964772d41ffbb7f3d0e6105f42bcc8dac9d0fc
TheBrightSparkDev <danharriesxd@gmail.com>
2023-01-23 02:53:32 +0000

    Correcting bugs in selection.js where the wrong div was being selected also added more automated tests and removed a test that was no longer needed

--- static/js/selection.js
+++ static/js/selection.js
@@ -1,26 +1,24 @@
 var debugselection = true
-// this came from this site: https://www.sitepoint.com/delay-sleep-pause-wait/#:~:text=The%20standard%20way%20of%20creating,()%20%3D%3E%20%7B%20console.
 // only used if debug is true
-// not used
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
+
+var beforeselectionList = $(".selectable")
+var selectionList = []
+for (var item of beforeselectionList){
+    selectionList.push(item.id)
 }
-var selectionList = $(".selectable")
 if (debugselection){
     console.log("selection.js connected")
     console.log("these html elements have been detected")
     console.log(selectionList)
+    console.log("thats a total of " + selectionList.length)
 }
-var selectionList = $(".selectable")
 window.addEventListener("load", function(event){
     if (debugselection){
         console.log("initialising!")
         checkBefore()
+        console.log("Look for mulitple mentions of the same element that means there has been a problem with the selectionlist")
     }
+    uniformSize()
     AddEvents()
     if (debugselection){
         console.log("Calibration test ready please pay attention to the html elements with the selectable tags and press spacebar when you are ready to run the calibration test")
@@ -28,18 +26,17 @@ window.addEventListener("load", function(event){
         document.body.onkeyup = function(e) {
             if (e.key == " " || e.code == "Space"){
                 elementTest()
-            } else if (e.key == "t" || e.code == "t"){
-                toggleTest()
             }
         }
     }
+    // corrects any errors created by uniform size (caused by the browser cached font awesome icon not loading quickly enough)
+    this.setTimeout(() =>{
+        if (sizeChecker()){
+            uniformSize()
+        }
+    },250)
 });
-function toggleTest(){
-    console.log("toggling all elements")
-    for (var element of selectionList){
-        toggle(element.id)
-    }
-}
+
 function checkBefore(){
     console.log("This is just a helpful note:")
     console.log("if you want the color to to be different when selected the css rules you want to target are .selectable:hover and .selected background-color and color")
@@ -50,10 +47,10 @@ function checkBefore(){
         console.log("Make sure this js file is called later in the DOM it is set to wait until DOM is loaded so this is unlikely to be the cause.")
     }
     for (var element of selectionList){
-        var el = document.getElementById(element.id)
+        var el = document.getElementById(element)
         // This is a test to see if the selected tag is already active on the element
         if (el.classList.value.includes("selected")){
-            console.log("element with ID: " + element.id + " Has already got the selected class added to it.")
+            console.log("element with ID: " + element + " Has already got the selected class added to it.")
             console.log("ignore this if that was intended of course!")
         }
     }
@@ -61,7 +58,7 @@ function checkBefore(){
 function elementTest(){
     console.log("detected elements should show blue")
     for (var element of selectionList){
-        var el = document.getElementById(element.id)
+        var el = document.getElementById(element)
         if (el.getAttribute('style') == 'background-color: blue !important'){
             el.setAttribute('style','');
         } else {
@@ -70,16 +67,46 @@ function elementTest(){
         }     
     }
 }
+// end of automated tests 
+
+function uniformSize(){
+    // this function resizes elements to give a uniform look
+    var sizes = []
+    for (var element of selectionList){
+        el = document.getElementById(element)
+        sizes.push(el.offsetHeight)
+    }
+    console.log(sizes)
+    var biggest = Math.max.apply(0,sizes) + "px"
+    for (var element of selectionList){
+        el = document.getElementById(element)
+        el.style.height = biggest
+    }
+}
+
 function AddEvents(){
     for (var element of selectionList){
-        el = document.getElementById(element.id)
-        el.addEventListener('click',() => {toggle(element.id)})
+        el = document.getElementById(element)
+        el.addEventListener('click',(e) => {toggle(e)})
         if (debugselection){
-            console.log("element with id: " + element.id + " should now be functioning")
+            console.log("element with id: " + element + " should now be functioning")
         }
     }
 }
-function toggle(id){
+function toggle(e){
+    // Note to self: There has to be a better way to do this...
+    // e is the pointer event
+    // sometimes when the div is clicked it puts through the div itself which the first if catches
+    // other times it sends a child element of the div which the else catches 
+    // one fix is to ensure the div contains only full size elements making it impossible to select the underlying div
+    // but this code is supposed to be extremely felxible as it will be reused in future projects on multiple selection pages.
+    for (var element of selectionList){
+        if (e.srcElement.id == element){
+            id = e.srcElement.id
+        } else if (e.srcElement.parentNode.id == element){
+            id = e.srcElement.parentNode.id
+        }
+    }
     el = document.getElementById(id)
     el.classList.toggle("selected")
     if(el.classList.value.includes("selected")){
@@ -97,4 +124,22 @@ function toggle(id){
             console.log("This runs if it doesn't detect the class selected is on the element")
         }
     }
+}
+function sizeChecker(){
+    var response = false
+    for (var element of selectionList){
+        el = document.getElementById(element)
+        var height = 0
+        for (var child of el.children){
+            console.log(child.offsetHeight)
+            height = child.offsetHeight + height
+        }
+        height = height + 24
+        console.log(height)
+        if (el.offsetHeight < height ){
+            el.style.height = height + "px"
+            response = true
+        }
+    }
+    return response
 }
