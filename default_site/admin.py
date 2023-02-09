8110c177774c73502281177a535338bdd64303a4
TheBrightSparkDev <danharriesxd@gmail.com>
2023-02-09 01:29:07 +0000

    feat - corrected a line to long error

--- default_site/admin.py
+++ default_site/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import (AdminFunctions, SecurityFunctions, ComplianceFunctions,
-                     EvolvingFunctions, Socials, ImportantOptions, ContactOptions)
+                     EvolvingFunctions, Socials, ImportantOptions,
+                     ContactOptions)
 
 # Register your models here.
 admin.site.register(AdminFunctions)
