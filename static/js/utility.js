882e74f2170b44de5a507bb2706a4e3be6188969
TheBrightSparkDev <danharriesxd@gmail.com>
2023-03-10 02:41:31 +0000

    feat - added javascript form validation to the form elements using utility.js

--- static/js/utility.js
+++ static/js/utility.js
@@ -67,58 +67,156 @@ function oneclickmanysubmits(element){
     let ignoreemptyfield = false
     let broken = false
     let empty = false
+    let emailaddress = false
+    let phonenumber = false
+    let submit = true
     for (el of elements){
         /* check if valid */
         childnodes = el.children
         for (childnode of childnodes){
+            let pelement = document.createElement("p");
+            pelement.setAttribute("class","errormessage")
             if (childnode.tagName == "INPUT"){
                 if (childnode.hasAttribute('required')){
                     if (childnode.value.length < 1){
                         empty = true
+                    } else {
+                        
                     }
                 }
                 if (childnode.getAttribute("name") == "question"){
                     if (childnode.value.length < 1){
-                        console.log(childnode)
-                        console.log("this is broken")
                         broken = true
                     } 
                 }
                 if (childnode.getAttribute("name") == "contactoptionID"){
                     if (childnode.value != element.getAttribute("contactoption")){
-                        console.log(childnode)
-                        console.log("this is broken")
-                        broken = true
+                        if (childnode.value.length < 1){
+                            broken = true
+                        } 
                     }
                 }
+                if (empty){
+                    // gets rid of last errormessage ready for new one
+                    if (childnode.nextElementSibling != null){
+                        childnode.nextElementSibling.remove()
+                    }
+                    let text = document.createTextNode("This is a required field ");
+                    pelement.appendChild(text);
+                    childnode.insertAdjacentElement('afterend', pelement)
+                    childnode.style.border = "thick solid #FF0000"
+                    valid = false
+                    empty = false
+                } 
+
+                if (childnode.getAttribute("type") == "email"){
+
+                    emailaddress = validemailaddress(childnode)
+
+                    if (!emailaddress){
+                        // gets rid of last errormessage ready for new one
+                        if (childnode.nextElementSibling != null){
+                            childnode.nextElementSibling.remove()
+                        }
+                        let text = document.createTextNode("This is not a valid email address ");
+                        pelement.appendChild(text);
+                        childnode.insertAdjacentElement('afterend', pelement)
+                        childnode.style.border = "thick solid #FF0000"
+                        valid = false
+                        empty = false
+                    } else {
+                        if (childnode.nextElementSibling != null){
+                            childnode.nextElementSibling.remove()
+                        }
+                    }
+
+                }
+                if (childnode.getAttribute("type") == "tel"){
+                    
+                    phonenumber = validphonenumber(childnode)
+
+                    if (!phonenumber){
+                        // gets rid of last errormessage ready for new one
+                        if (childnode.nextElementSibling != null){
+                            childnode.nextElementSibling.remove()
+                        }
+                        let text = document.createTextNode("This is not a valid phone number ");
+                        pelement.appendChild(text);
+                        childnode.insertAdjacentElement('afterend', pelement)
+                        childnode.style.border = "thick solid #FF0000"
+                        valid = false
+                        empty = false
+                    } else {
+                        if (childnode.nextElementSibling != null){
+                            childnode.nextElementSibling.remove()
+                        }
+                    }
+                    
+    
+                }
+
             } else if (childnode.tagName == "LABEL"){
                 label = childnode.textContent
                 console.log(label)
             }
-            if (empty){
-                childnode.style.border = "thick solid #FF0000"
-                console.log("this submit is invalid due to an empty input")
-                valid = false
-                empty = false
-            } 
-            
         }
+
         if (!broken){
             if (valid){
-                /*el.submit()*/
+                if (!phonenumber && !emailaddress){
+                    console.log("successfully inputted all correctly")
+                    if (childnode.nextElementSibling != null){
+                        childnode.nextElementSibling.remove()
+                    }
+                } else {
+                    submit = false
+                }
             } else {
                 if (!ignoreemptyfield){
                     alert("please answer all required questions")
                     ignoreemptyfield = true
+                    submit = false
                 }
-            }
-            
+            }  
         } else {
             if (!ignorehiddeninputedit){
-                alert("please dont edit any hidden form elements")
+                alert("please dont edit any hidden form elements you will have to refresh the page to fix this issue")
+                submit = false
             }
             ignorehiddeninputedit = true
+            submit = false
         }
     }   
+    if (submit){
+        console.log("successfully submitted")
+        for (el of elements){
+            //el.submit()
+        }
+    }
 
 }
+/* the following regex forms were created using https://regexr.com/ brilliant tool community patterns is where these came from */
+function validphonenumber(element){
+    console.log(element.value)
+    var phonenoregex = new RegExp(/\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*/g);
+    if(phonenoregex.test(element.value)) {
+        console.log("valid number")
+      return true;
+    }  
+    else {  
+      return false;
+    }
+}
+
+function validemailaddress(element){
+    console.log(element.value)
+    var emailREGEX = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g);
+    if(emailREGEX.test(element.value)) {
+        console.log("valid email")
+      return true;
+    }  
+    else {  
+      return false;
+    }
+}
+
